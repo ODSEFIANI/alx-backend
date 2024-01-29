3fc64c6e6850425307a3671cbb8794753402a692
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-01-29 23:16:26 +0100

    2. Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+'''pagination
+'''
+import csv
+from typing import List, Tuple, Dict
+import math
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page_number: int = 1, items_per_page: int = 10) -> List[List]:
+        assert isinstance(page_number, int)
+        assert isinstance(items_per_page, int)
+        assert page_number > 0
+        assert items_per_page > 0
+        csv_size = len(self.dataset())
+        start_index, end_index = index_range(page_number, items_per_page)
+        end_index = min(end_index, csv_size)
+        if start_index >= csv_size:
+            return []
+        return self.dataset()[start_index:end_index]
+
+    def get_hyper(self, page_number: int = 1, items_per_page: int = 10) -> Dict[str, any]:
+        assert isinstance(page_number, int)
+        assert isinstance(items_per_page, int)
+        assert page_number > 0
+        assert items_per_page > 0
+
+        page_data = self.get_page(page_number, items_per_page)
+        csv_size = len(self.dataset())
+        total_pages = math.ceil(csv_size / items_per_page)
+
+        next_page = page_number + 1 if (page_number * items_per_page) < csv_size else None
+        prev_page = page_number - 1 if page_number > 1 else None
+
+        return {
+            "page_size": len(page_data),
+            "page": page_number,
+            "data": page_data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    return ((page - 1) * page_size, page * page_size)
