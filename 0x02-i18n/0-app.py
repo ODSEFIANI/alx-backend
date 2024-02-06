9151f9d67a7a8f94e485924bf2859d7137281af9
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-06 19:39:40 +0100

    0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Route for the home page.
+
+    Returns:
+        str: The rendered HTML content.
+    """
+    return render_template(
+            '0-index.html',
+            title='Welcome to Holberton',
+            header='Hello world')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
