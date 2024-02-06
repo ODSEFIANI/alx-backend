3916b54f9f89ce805d14a5f8fbc73f2cc8939071
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-06 20:01:10 +0100

    1. Basic Babel setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """
     Route for the home page.
